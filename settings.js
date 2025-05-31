be03bee09304843032013a4a6a5eff15e050e0c6
tecca-bit478 <tecca5387@gmail.com>
2025-05-31 16:30:20 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'cornelius', // Your name
-  ownerNumber: '254797348876', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2540797348876', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
